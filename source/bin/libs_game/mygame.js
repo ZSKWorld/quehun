c8574eb3dd9088cc17eababba547621e1eaa51e0
ake <1052938743@qq.com>
2025-11-03 10:53:45 +0800

    缩进空格

--- source/bin/libs_game/mygame.js
+++ source/bin/libs_game/mygame.js
@@ -5,70 +5,70 @@ const $windowImmit = function (name, obj) { window[name] = obj; }
 const ExtensionClass = function (cls) { return cls; }
 
 const setEvent = function (name, list, func, once, args) {
-    if (list.indexOf(func) >= 0) return;
-    list.push(func);
-    if (once) {
-        func[name] ||= {};
-        func[name].__once = once;
-    }
-    if (args) {
-        func[name] ||= {};
-        func[name].__args = args;
-    }
+	if (list.indexOf(func) >= 0) return;
+	list.push(func);
+	if (once) {
+		func[name] ||= {};
+		func[name].__once = once;
+	}
+	if (args) {
+		func[name] ||= {};
+		func[name].__args = args;
+	}
 }
 
 const ViewKeyEvent = function (name, key = -1, once = false, args = null) {
-    return function (target, propertyKey, descriptor) {
-        target.__viewKeyEventMap ||= {};
-        target.__viewKeyEventMap[name] ||= {};
-        target.__viewKeyEventMap[name][key] ||= [];
+	return function (target, propertyKey, descriptor) {
+		target.__viewKeyEventMap ||= {};
+		target.__viewKeyEventMap[name] ||= {};
+		target.__viewKeyEventMap[name][key] ||= [];
 
-        const func = descriptor.value;
-        const list = target.__viewKeyEventMap[name][key];
-        setEvent(key, list, func, once, args);
-    }
+		const func = descriptor.value;
+		const list = target.__viewKeyEventMap[name][key];
+		setEvent(key, list, func, once, args);
+	}
 }
 
 const ViewMouseEvent = function (name, once, args) {
-    return function (target, propertyKey, descriptor) {
-        target.__viewMouseEventMap ||= {};
-        target.__viewMouseEventMap[name] ||= [];
+	return function (target, propertyKey, descriptor) {
+		target.__viewMouseEventMap ||= {};
+		target.__viewMouseEventMap[name] ||= [];
 
-        const func = descriptor.value;
-        const list = target.__viewMouseEventMap[name];
-        setEvent(name, list, func, once, args);
-    }
+		const func = descriptor.value;
+		const list = target.__viewMouseEventMap[name];
+		setEvent(name, list, func, once, args);
+	}
 }
 
 const ViewEvent = function (name, once, args) {
-    return function (target, propertyKey, descriptor) {
-        target.__viewEventMap ||= {};
-        target.__viewEventMap[name] ||= [];
+	return function (target, propertyKey, descriptor) {
+		target.__viewEventMap ||= {};
+		target.__viewEventMap[name] ||= [];
 
-        const func = descriptor.value;
-        const list = target.__viewEventMap[name];
-        setEvent(name, list, func, once, args);
-    }
+		const func = descriptor.value;
+		const list = target.__viewEventMap[name];
+		setEvent(name, list, func, once, args);
+	}
 }
 
 const InterestNotify = function (eventName, once, args) {
-    return function (target, propertyKey, descriptor) {
-        target.__notifyMap ||= {};
-        target.__notifyMap[eventName] ||= [];
+	return function (target, propertyKey, descriptor) {
+		target.__notifyMap ||= {};
+		target.__notifyMap[eventName] ||= [];
 
-        const func = descriptor.value;
-        const list = target.__notifyMap[eventName];
-        setEvent(eventName, list, func, once, args);
-    };
+		const func = descriptor.value;
+		const list = target.__notifyMap[eventName];
+		setEvent(eventName, list, func, once, args);
+	};
 }
 
 const InterestMessage = function (msgId, once, args) {
-    return function (target, propertyKey, descriptor) {
-        target.__messageMap ||= {};
-        target.__messageMap[msgId] ||= [];
+	return function (target, propertyKey, descriptor) {
+		target.__messageMap ||= {};
+		target.__messageMap[msgId] ||= [];
 
-        const func = descriptor.value;
-        const list = target.__messageMap[msgId];
-        setEvent(msgId, list, func, once, args);
-    };
+		const func = descriptor.value;
+		const list = target.__messageMap[msgId];
+		setEvent(msgId, list, func, once, args);
+	};
 }
--- source/bin/libs_leb/enum_define.d.js
+++ source/bin/libs_leb/enum_define.d.js
@@ -1,72 +1,72 @@
 /** 本地化语言 */
 ELanguage = {
-    CHS : "chs",
-    CHST : "chs_t",
-    EN: "en",
-    JP: "jp",
-    KR: "kr",
+	CHS: "chs",
+	CHST: "chs_t",
+	EN: "en",
+	JP: "jp",
+	KR: "kr",
 }
 
 /** 行政版本 */
 EClientType = {
-    CHS : "chs",
-    CHST : "chs_t",
-    EN: "en",
-    JP: "jp",
-    KR: "kr",
+	CHS: "chs",
+	CHST: "chs_t",
+	EN: "en",
+	JP: "jp",
+	KR: "kr",
 }
 
 /** 本地数据类型 */
 ELocalDataKey = {
-    /** 自动登录 */
-    AutoLogin: "LocalDataKey_AutoLogin",
-    /** 上次登录账号 */
-    LastLoginData: "LocalDataKey_LastLoginData",
-    /** 设备id */
-    DeviceId: "LocalDataKey_DeviceId",
-    /** 重复登陆 */
-    MultiLogin: "LocalDataKey_MultiLogin",
+	/** 自动登录 */
+	AutoLogin: "LocalDataKey_AutoLogin",
+	/** 上次登录账号 */
+	LastLoginData: "LocalDataKey_LastLoginData",
+	/** 设备id */
+	DeviceId: "LocalDataKey_DeviceId",
+	/** 重复登陆 */
+	MultiLogin: "LocalDataKey_MultiLogin",
 }
 
 /** 页面类型 */
 EViewType = {
-    UI: "UI",
-    Component: "Component",
-    Render: "Render",
-    Button: "Button",
+	UI: "UI",
+	Component: "Component",
+	Render: "Render",
+	Button: "Button",
 }
 
 /** UI层级 */
 ELayer = {
-    Scene: "Scene",
-    UIBottom: "UIBottom",
-    UIMiddle: "UIMiddle",
-    UITop: "UITop",
-    Dialog: "Dialog",
-    Alert: "Alert",
-    Lock: "Lock",
+	Scene: "Scene",
+	UIBottom: "UIBottom",
+	UIMiddle: "UIMiddle",
+	UITop: "UITop",
+	Dialog: "Dialog",
+	Alert: "Alert",
+	Lock: "Lock",
 }
 
 /** 按键事件类型 */
 EKeyEventType = {
-    KeyDown: "keydown",
-    KeyPress: "keypress",
-    KeyUp: "keyup",
+	KeyDown: "keydown",
+	KeyPress: "keypress",
+	KeyUp: "keyup",
 }
 
 /** 鼠标事件类型 */
 EMouseEventType = {
-    MouseDown: "mousedown",
-    MouseUp: "mouseup",
-    MouseMove: "mousemove",
-    MouseClick: "click",
-    MouseDoubleClick: "doubleclick",
-    MouseRightClick: "rightclick",
-    RightMouseDown: "rightmousedown",
-    RightMouseUp: "rightmouseup",
-    MouseOver: "mouseover",
-    MouseOut: "mouseout",
-    MouseWheel: "mousewheel",
-    MouseDrag: "mousedrag",
-    MouseDragEnd: "mousedragend",
+	MouseDown: "mousedown",
+	MouseUp: "mouseup",
+	MouseMove: "mousemove",
+	MouseClick: "click",
+	MouseDoubleClick: "doubleclick",
+	MouseRightClick: "rightclick",
+	RightMouseDown: "rightmousedown",
+	RightMouseUp: "rightmouseup",
+	MouseOver: "mouseover",
+	MouseOut: "mouseout",
+	MouseWheel: "mousewheel",
+	MouseDrag: "mousedrag",
+	MouseDragEnd: "mousedragend",
 }

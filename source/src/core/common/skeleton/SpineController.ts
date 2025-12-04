4a23c26c5cb96052d4b410f3fdb0fd8c6a04ef68
ake <1052938743@qq.com>
2025-12-04 18:50:06 +0800

    推进

--- source/src/core/common/skeleton/SpineController.ts
+++ source/src/core/common/skeleton/SpineController.ts
@@ -1,23 +1,18 @@
 export class SpineController extends Laya.Script implements ISpineController {
 	override owner: Laya.Sprite;
 	private _spineId: number = 0;
-	private _spineNodes: Laya.Spine2DRenderNode[];
+	private _spineNodes: Laya.Spine2DRenderNode[] = [];
 	get spineId() { return this._spineId; }
-	get spineNodes() {
-		if (!this._spineNodes && !this.destroyed) {
-			this._spineNodes = [];
-			const childs = this.gowner._children;
-			for (let i = 0; i < childs.length; i++) {
-				const node = childs[i].getComponent(Laya.Spine2DRenderNode);
-				if (!node) continue;
-				this._spineNodes.push(node);
-			}
-		}
-		return this._spineNodes;
-	}
+	get spineNodes() { return this._spineNodes; }
 
 	init(id: number) {
 		this._spineId = id;
+		const childs = this.gowner._children;
+		for (let i = 0; i < childs.length; i++) {
+			const node = childs[i].getComponent(Laya.Spine2DRenderNode);
+			if (!node) continue;
+			this._spineNodes.push(node);
+		}
 	}
 
 	play(nameOrIndex: string | number, loop: boolean, force = true, start = 0, end = 0, freshSkin = true, playAudio = false) {
@@ -29,7 +24,7 @@ export class SpineController extends Laya.Script implements ISpineController {
 	}
 
 	override onDestroy() {
-		this._spineNodes = null;
+		this._spineNodes.length = 0;
 	}
 
 	override onReset() {

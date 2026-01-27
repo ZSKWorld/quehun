eeb6144ac77c7b991baa988e0e81539f08bbd0ab
zsk <1052938743@qq.com>
2026-01-28 00:35:28 +0800

    save

--- source/src/core/ui/view/PkgCommon/view/coms/ComItemView.ts
+++ source/src/core/ui/view/PkgCommon/view/coms/ComItemView.ts
@@ -9,20 +9,24 @@ export class ComItemView extends ExtensionClass<IView, ComItem>(ComItem) impleme
 
 	}
 
+	/** 刷新指定路径icon */
 	refreshSkin(url: string) {
 		this.loader_icon.icon = url;
 	}
 
+	/** 刷新半透明icon */
 	refreshIcon(id: number) {
 		const iconPath = $itemUtil.getItemView(id).icon;
 		$dynamicResMgr.setLoader(this.loader_icon, iconPath);
 	}
 
+	/** 刷新不透明icon */
 	refreshItemIcon(id: number) {
 		const iconPath = $itemUtil.getItemView(id).itemIcon;
 		$dynamicResMgr.setLoader(this.loader_icon, iconPath);
 	}
 
+	/** 刷新loading图 */
 	refreshLoadingImage(id: number) {
 		const cfgCg = $cfgMgr.item_definition.loading_image[id];
 		const iconPath = cfgCg ? cfgCg.thumb_path : "";

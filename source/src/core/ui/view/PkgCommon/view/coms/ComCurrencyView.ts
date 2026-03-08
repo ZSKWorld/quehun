ed058da4e1d930a1a022beac9bef23b4ba6150aa
zsk <1052938743@qq.com>
2026-03-08 23:32:23 +0800

    save

--- source/src/core/ui/view/PkgCommon/view/coms/ComCurrencyView.ts
+++ source/src/core/ui/view/PkgCommon/view/coms/ComCurrencyView.ts
@@ -8,21 +8,20 @@ export const enum EComCurrencyMsg {
 export class ComCurrencyView extends ExtensionClass<IView, ComCurrency>(ComCurrency) implements IView {
 
 	override onCreate() {
-		const { btn_add, btn_currency } = this;
+		const { btn_add, btn_currency, ctrl_type } = this;
 		btn_add.onClick(this, this.sendEvent, [EComCurrencyMsg.OnBtnAddClick]);
 		btn_currency.onClick(this, this.sendEvent, [EComCurrencyMsg.OnBtnCurrencyClick]);
-	}
-
-	/**
-	 * 设置货币类型
-	 * @param type 货币类型
-	 * - 0:金币
-	 * - 1:辉玉
-	 * - 2:皮肤券
-	 * - 3:寻觅券
-	 * - 4:信仰值
-	 */
-	setCurrencyType(type: 0 | 1 | 2 | 3 | 4) {
-		this.ctrl_type.selectedIndex = type;
+		switch (ctrl_type.selectedIndex) {
+			// 金币
+			case 0: break;
+			// 魂玉
+			case 1: break;
+			// 皮肤券
+			case 2: break;
+			// 寻觅券
+			case 3: break;
+			// 信仰值
+			case 4: break;
+		}
 	}
 }

6f33127dd4afe738e6b7019f2559af59e1744383
zsk <1052938743@qq.com>
2026-03-17 22:48:43 +0800

    save

--- source/src/core/ui/view/PkgMain/mediator/uis/UIVideoMediator.ts
+++ source/src/core/ui/view/PkgMain/mediator/uis/UIVideoMediator.ts
@@ -17,17 +17,29 @@ export class UIVideoMediator extends MediatorBase<UIVideoView, IUIVideoData> {
 		this._video = new Laya.VideoNode();
 		this.view.displayObject.addChild(this._video);
 		this._video.autoPlay = false;
+		this._video.loop = true;
 		this._video.allowBackground = true;
 		this._video.options.objectFit = "fill";
 		this._video.size(Laya.stage.width, Laya.stage.height);
 		this._video.source = ResPath.EUnclassifiedPath[400107];
+		const player = this._video.player as Laya.VideoTexture;
+		player.on(Laya.Event.READY, this, () => {
+			Logger.error("ready", Laya.timer.currFrame);
+		});
+		player.on("canplay", this, () => {
+			Logger.error("canplay", Laya.timer.currFrame);
+		});
+		player.on("ended", this, () => {
+			Logger.error("ended", Laya.timer.currFrame);
+		});
 		this._video.play();
+		Logger.error("play", Laya.timer.currFrame);
 	}
 
-	override onUpdate() {
-		if (this._video.readyState == 4) return;
-		Logger.error(this._video.readyState);
-	}
+	// override onUpdate() {
+	// 	if (this._video.readyState == 4) return;
+	// 	Logger.error(this._video.readyState);
+	// }
 
 	override onDisable() {
 		this._video.destroy();

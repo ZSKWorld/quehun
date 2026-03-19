2809030e7dd280ba63c53ffdc1cf9996716dd742
zsk <1052938743@qq.com>
2026-03-19 23:31:20 +0800

    save

--- source/src/core/ui/ui/PkgMain/UIVideo.ts
+++ source/src/core/ui/ui/PkgMain/UIVideo.ts
@@ -2,7 +2,10 @@
 
 export default class UIVideo extends fgui.GComponent {
 
+	protected com_videoRoot: fgui.GComponent;
 	protected btn_jump: fgui.GButton;
+	protected trans_show: fgui.Transition;
+	protected trans_hide: fgui.Transition;
 	public static url: string = "ui://vith2b66rmm9obh3";
 
 	public static createInstance(): UIVideo {
@@ -10,6 +13,9 @@ export default class UIVideo extends fgui.GComponent {
 	}
 
 	protected override onConstruct(): void {
-		this.btn_jump = <fgui.GButton>(this.getChildAt(0));
+		this.com_videoRoot = <fgui.GComponent>(this.getChildAt(1));
+		this.btn_jump = <fgui.GButton>(this.getChildAt(4));
+		this.trans_show = this.getTransitionAt(0);
+		this.trans_hide = this.getTransitionAt(1);
 	}
 }

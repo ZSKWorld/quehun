be241e6391a9805ad2f7e86d4b155f1e7b8d7a59
zsk <1052938743@qq.com>
2026-03-28 00:30:10 +0800

    save

--- source/src/core/ui/view/PkgMain/mediator/uis/UIVideoMediator.ts
+++ source/src/core/ui/view/PkgMain/mediator/uis/UIVideoMediator.ts
@@ -20,25 +20,25 @@ export class UIVideoMediator extends MediatorBase<UIVideoView, IUIVideoData> {
 		video.size(Laya.stage.width, Laya.stage.height);
 		video.source = ResPath.EUnclassifiedPath[400107];
 		// video.play();
-		video.on(EVideoLoadEvent.LoadStart, this, () => Logger.error("LoadStart"))
-		video.on(EVideoLoadEvent.LoadedMetadata, this, () => Logger.error("LoadedMetadata"))
-		video.on(EVideoLoadEvent.LoadedData, this, () => Logger.error("LoadedData"))
-		video.on(EVideoLoadEvent.Progress, this, () => Logger.error("Progress"))
-		video.on(EVideoLoadEvent.CanPlay, this, () => Logger.error("CanPlay"))
-		video.on(EVideoLoadEvent.CanPlayThrough, this, () => Logger.error("CanPlayThrough"))
-		video.on(EVideoPlaybackEvent.Play, this, () => Logger.error("Play"))
-		video.on(EVideoPlaybackEvent.Playing, this, () => Logger.error("Playing"))
-		video.on(EVideoPlaybackEvent.Pause, this, () => Logger.error("Pause"))
-		video.on(EVideoPlaybackEvent.Ended, this, () => Logger.error("Ended"))
-		video.on(EVideoPlaybackEvent.Waiting, this, () => Logger.error("Waiting"))
-		video.on(EVideoPlaybackEvent.Stalled, this, () => Logger.error("Stalled"))
-		video.on(EVideoProgressAndInteractionEvent.TimeUpdate, this, () => Logger.error("TimeUpdate"))
-		video.on(EVideoProgressAndInteractionEvent.Seeking, this, () => Logger.error("Seeking"))
-		video.on(EVideoProgressAndInteractionEvent.Seeked, this, () => Logger.error("Seeked"))
-		video.on(EVideoProgressAndInteractionEvent.VolumeChange, this, () => Logger.error("VolumeChange"))
-		video.on(EVideoProgressAndInteractionEvent.RateChange, this, () => Logger.error("RateChange"))
-		video.on(EVideoErrorEvent.Error, this, () => Logger.error("Error"))
-		video.on(EVideoErrorEvent.Abort, this, () => Logger.error("Abort"))
+		video.on(EVideoLoadEvent.LoadStart, this, () => Logger.error("LoadStart"));
+		video.on(EVideoLoadEvent.LoadedMetadata, this, () => Logger.error("LoadedMetadata"));
+		video.on(EVideoLoadEvent.LoadedData, this, () => Logger.error("LoadedData"));
+		video.on(EVideoLoadEvent.Progress, this, () => Logger.error("Progress"));
+		video.on(EVideoLoadEvent.CanPlay, this, () => Logger.error("CanPlay"));
+		video.on(EVideoLoadEvent.CanPlayThrough, this, () => Logger.error("CanPlayThrough"));
+		video.on(EVideoPlaybackEvent.Play, this, () => Logger.error("Play"));
+		video.on(EVideoPlaybackEvent.Playing, this, () => Logger.error("Playing"));
+		video.on(EVideoPlaybackEvent.Pause, this, () => Logger.error("Pause"));
+		video.on(EVideoPlaybackEvent.Ended, this, () => Logger.error("Ended"));
+		video.on(EVideoPlaybackEvent.Waiting, this, () => Logger.error("Waiting"));
+		video.on(EVideoPlaybackEvent.Stalled, this, () => Logger.error("Stalled"));
+		video.on(EVideoProgressAndInteractionEvent.TimeUpdate, this, () => Logger.error("TimeUpdate"));
+		video.on(EVideoProgressAndInteractionEvent.Seeking, this, () => Logger.error("Seeking"));
+		video.on(EVideoProgressAndInteractionEvent.Seeked, this, () => Logger.error("Seeked"));
+		video.on(EVideoProgressAndInteractionEvent.VolumeChange, this, () => Logger.error("VolumeChange"));
+		video.on(EVideoProgressAndInteractionEvent.RateChange, this, () => Logger.error("RateChange"));
+		video.on(EVideoErrorEvent.Error, this, () => Logger.error("Error"));
+		video.on(EVideoErrorEvent.Abort, this, () => Logger.error("Abort"));
 	}
 
 

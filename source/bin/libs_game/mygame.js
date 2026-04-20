a6c65a1a243f623291db7ad2b67c0cf7c15bc797
ZSKWorld <1052938743@qq.com>
2026-04-20 16:32:15 +0800

    save

--- source/bin/libs_game/mygame.js
+++ source/bin/libs_game/mygame.js
@@ -1,8 +1,8 @@
 //注入全局变量
-const $windowImmit = function (name, obj) { window[name] = obj; }
+function $windowImmit(name, obj) { window[name] = obj; }
 
 //用于扩展类字段，在外部定义的字段在内部可读，扩展的字段或方法不能在构造期间调用
-const ExtensionClass = function (cls) { return cls; }
+function ExtensionClass(cls) { return cls; };
 
 function Singleton() {
 	return class Singleton {
@@ -13,7 +13,7 @@ function Singleton() {
 	};
 }
 
-const setEvent = function (name, list, func, once, args) {
+function setEvent(name, list, func, once, args) {
 	if (list.indexOf(func) >= 0) return;
 	list.push(func);
 	if (once) {
@@ -24,9 +24,9 @@ const setEvent = function (name, list, func, once, args) {
 		func[name] ||= {};
 		func[name].__args = args;
 	}
-}
+};
 
-const ViewKeyEvent = function (name, key = -1, once = false, args = null) {
+function ViewKeyEvent(name, key = -1, once = false, args = null) {
 	return function (target, propertyKey, descriptor) {
 		target.__viewKeyEventMap ||= {};
 		target.__viewKeyEventMap[name] ||= {};
@@ -35,10 +35,10 @@ const ViewKeyEvent = function (name, key = -1, once = false, args = null) {
 		const func = descriptor.value;
 		const list = target.__viewKeyEventMap[name][key];
 		setEvent(key, list, func, once, args);
-	}
-}
+	};
+};
 
-const ViewMouseEvent = function (name, once, args) {
+function ViewMouseEvent(name, once, args) {
 	return function (target, propertyKey, descriptor) {
 		target.__viewMouseEventMap ||= {};
 		target.__viewMouseEventMap[name] ||= [];
@@ -46,10 +46,10 @@ const ViewMouseEvent = function (name, once, args) {
 		const func = descriptor.value;
 		const list = target.__viewMouseEventMap[name];
 		setEvent(name, list, func, once, args);
-	}
-}
+	};
+};
 
-const ViewEvent = function (name, once, args) {
+function ViewEvent(name, once, args) {
 	return function (target, propertyKey, descriptor) {
 		target.__viewEventMap ||= {};
 		target.__viewEventMap[name] ||= [];
@@ -57,10 +57,10 @@ const ViewEvent = function (name, once, args) {
 		const func = descriptor.value;
 		const list = target.__viewEventMap[name];
 		setEvent(name, list, func, once, args);
-	}
-}
+	};
+};
 
-const InterestNotify = function (eventName, once, args) {
+function InterestNotify(eventName, once, args) {
 	return function (target, propertyKey, descriptor) {
 		target.__notifyMap ||= {};
 		target.__notifyMap[eventName] ||= [];
@@ -69,9 +69,9 @@ const InterestNotify = function (eventName, once, args) {
 		const list = target.__notifyMap[eventName];
 		setEvent(eventName, list, func, once, args);
 	};
-}
+};
 
-const InterestMessage = function (msgName, once, args) {
+function InterestMessage(msgName, once, args) {
 	return function (target, propertyKey, descriptor) {
 		target.__messageMap ||= {};
 		target.__messageMap[msgName] ||= [];
@@ -80,9 +80,9 @@ const InterestMessage = function (msgName, once, args) {
 		const list = target.__messageMap[msgName];
 		setEvent(msgName, list, func, once, args);
 	};
-}
+};
 
-const InterestUserEvent = function (eventName, once, args) {
+function InterestUserEvent(eventName, once, args) {
 	return function (target, propertyKey, descriptor) {
 		target.__userEventMap ||= {};
 		target.__userEventMap[eventName] ||= [];
@@ -91,4 +91,4 @@ const InterestUserEvent = function (eventName, once, args) {
 		const list = target.__userEventMap[eventName];
 		setEvent(eventName, list, func, once, args);
 	};
-}
+};

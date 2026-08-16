77c03de5f79bab545a71e1fad8cde42278904b62
zsk <1052938743@qq.com>
2026-08-16 16:40:16 +0800

    save

--- source/src/core/mvc/Interface.d.ts
+++ source/src/core/mvc/Interface.d.ts
@@ -68,12 +68,13 @@ declare interface IObserver extends INotifier {
 
 }
 
-/**页面扩展 */
-declare interface IViewExtend {
+/**页面 */
+declare interface IView extends fgui.GComponent {
 	readonly viewId: EViewID;
 	readonly viewType: EViewType;
 	readonly viewLayer: ELayer;
 	readonly viewCategory: EViewCategory;
+	readonly mediator: IMediator;
 
 	/**
 	 * 派发全局事件
@@ -97,11 +98,6 @@ declare interface IViewExtend {
 
 	/** 移除当前页面，只有UI界面才能移除自身，其他Com，Btn，Render之类的无效 */
 	closeSelf(): Promise<void>;
-}
-
-/**页面 */
-declare interface IView extends fgui.GComponent, IViewExtend {
-	readonly mediator: IMediator;
 
 	/**
 	 ** 页面创建完毕之后执行，只执行一次。
@@ -151,12 +147,39 @@ declare interface IView extends fgui.GComponent, IViewExtend {
 }
 
 /**中介类 */
-declare interface IMediator<V extends IView = IView, D = any> extends Laya.Script, IViewExtend {
+declare interface IMediator<V extends IView = IView, D = any> extends Laya.Script {
+	readonly viewId: EViewID;
+	readonly viewType: EViewType;
+	readonly viewLayer: ELayer;
+	readonly viewCategory: EViewCategory;
 	readonly owner: Laya.Sprite;
 	/** 页面数据 */
 	data: D;
 	/** 控制器挂载的ui页面 */
 	get view(): V;
+
+	/**
+	 * 派发全局事件
+	 * @param eventName 
+	 * @param data （可选）回调数据。注意：如果是需要传递多个参数 p1,p2,p3,...可以使用数组结构如：[p1,p2,p3,...] ；如果需要回调单个参数 p ，且 p 是一个数组，则需要使用结构如：[p]，其他的单个参数 p ，可以直接传入参数 p。
+	 */
+	dispatch(eventName: string, data?: any): void;
+
+	/**
+	 * 打开页面
+	 * @param viewId 页面id
+	 * @param data 传入参数, default: null
+	 * @param openType 页面打开对当前页面操作的类型, default: {@link EViewOpenType.None}
+	 */
+	openView<T = any>(viewId: EUIViewID, data?: T, openType?: EViewOpenType): Promise<void>;
+
+	/** 移除页面
+	 * @param viewId 页面id
+	 */
+	closeView(viewId: EUIViewID): Promise<void>;
+
+	/** 移除当前页面，只有UI界面才能移除自身，其他Com，Btn，Render之类的无效 */
+	closeSelf(): Promise<void>;
 
 	/** 添加页面事件 */
 	addEvent(type: string, listener: Function, args?: any[], once?: boolean): void;
